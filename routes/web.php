36bc49b8fcc6d7565a8ca7258306beed8bf62dd9
Jose Montoya <montoyaguzman7@gmail.com>
2021-01-26 00:05:48 -0600

    FEAT : Integra componente controller, rutas y nombre de la BD

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\UserController;
 |
 */
 
-
-Route::get('/', function() { 
-    return view('welcome');
-});
+Route::get('/', [UserController::class, 'index']);
+Route::post('users', [UserController::class, 'store']);
+Route::delete('users/{user}', [UserController::class, 'destroy']);
