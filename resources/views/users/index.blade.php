83ad0c301087a69c98f8132023e03878e422d370
Jose Montoya <montoyaguzman7@gmail.com>
2021-01-26 20:44:01 -0600

    FEAT : Agrega logica al controller, vista y funcionalidad de eliminado

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 mx-auto">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                                <tr>
+                                    <th>{{$user->id}}</th>
+                                    <th>{{$user->nombre}}</th>
+                                    <th>{{$user->email}}</th>
+                                    <th>
+                                        <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                            <input 
+                                                type="submit" 
+                                                value="Eliminar" 
+                                                class="btn btn-sm btn-danger"
+                                                onclick="return confirm('¿Seguro?')"
+                                                >
+                                        </form>
+                                    </th>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
