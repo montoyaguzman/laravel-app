76a292e9d36cab5a6a06727a6e38610ddd578a75
Jose Montoya <montoyaguzman7@gmail.com>
2021-02-01 16:06:39 -0600

    FEAT : Agrega bcrypt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -27,7 +27,7 @@ class UserController extends Controller
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password
+            'password' => bcrypt($request->password)
         ]);
         return back();
     }
